09335c629b48b241e99efca061e9b4f260fc9272
Demitri Cameron <oblivion9794@gmail.com>
2023-01-19 15:53:21 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise;
+public class WordSmith
+{
+       public bool IsAPalindrome(string word)
+        {
+            string reverseWord = "";
+            
+            for (int i = word.Length-1; i >= 0; i--)
+            {
+            reverseWord += word[i];
+            }
+        return (reverseWord.ToLower() == word.ToLower());
+
+
+            //if (reverseWord == word)
+            //{
+            //    return true;
+            //}
+            //else
+            //{ 
+            //return false;
+            //}
+        {
+    }
+}
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("dEmitri",false)]
+        [InlineData("breAd",false)]
+        [InlineData("Mom",true)]
+        [InlineData("Shave",false)]
+        [InlineData("always",false)]
+        [InlineData("motel",false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
-
+            //Arrange
+            var instance = new WordSmith();
+            //Act
+            var actual = instance.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
